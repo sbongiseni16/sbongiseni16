377965784a33bf793e3bb75f1ba35ce05fba5e47
Xolani Maiko <xolanimaiko@gmail.com>
2024-08-06 22:25:25 +0200

    Exercise 5.1

--- src/stopwatch/StopWatch.h
+++ src/stopwatch/StopWatch.h
@@ -1,13 +1,31 @@
-#include <iostream>
-#include <ctime>
+#ifndef STOPWATCH_H
+#define STOPWATCH_H
 
-class stopwatch{
-    public:
-        stopwatch();
-        double getProcessTime();
-        void setProcessTime();
-    private:
-        double processTime;
+// StopWatch class for measuring elapsed time
+class StopWatch {
+public:
+    // Constructor to initialize the stopwatch
+    StopWatch();
 
+    // Start the stopwatch
+    void begin();
+
+    // Stop the stopwatch
+    void end();
+
+    // Reset the stopwatch to zero
+    void reset();
+
+    // Fetch the elapsed time in seconds
+    double fetchElapsed_time() const;
+
+private:
+    double begin_time;  // Store the start time of the stopwatch
+    double end_time;    // Store the end time of the stopwatch
+    bool running;       // Flag to check if the stopwatch is currently running
+
+    // Get the current process time in seconds
+    double getProcessTime() const;
 };
 
+#endif // STOPWATCH_H
